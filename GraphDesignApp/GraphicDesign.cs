975d1dfe77e81cd105926be329c0ccabadb442c1
Prachi Jha <prachichoudhari@gmail.com>
2019-03-24 09:04:36 -0700

    Adding Classes and properties

---
+++ GraphDesignApp/GraphicDesign.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphDesignApp
+{
+    class GraphicDesign
+    {
+        #region Properties
+
+        public GraphicDesignType DesignType { get; set; }
+        public GraphicDesignColor Color { get; set; }
+        public GraphicDesignSize Size { get; set; }
+        public DesignPaperQuality PaperQuality { get; set; }
+        public ShippingType ShippingType { get; set; }
+
+        #endregion
+    }
+
+    public enum GraphicDesignType
+    {
+        Flyer,
+        InvitationCard,
+        SocialMediaFlyer,
+    }
+
+    public enum GraphicDesignColor
+    {
+        Red,
+        Blue,
+        Green,       
+        Yellow,
+        Orange,
+    }
+
+    public enum GraphicDesignSize
+    {
+        Large,
+        Medium,
+        Small,
+        ExtraSmall,
+    }
+    
+    public enum DesignPaperQuality
+    {
+        Matte,
+        Glossy,
+        Regular,
+        Card,
+    }
+
+    public enum ShippingType
+    {
+        Delivery,
+        PickUp,
+    }
+}
+
--- GraphDesignApp/Program.cs
+++ GraphDesignApp/Program.cs
@@ -6,15 +6,7 @@ namespace GraphDesignApp
     {
         static void Main(string[] args)
         {
-            #region Properties
-
-            pubilc string TypeOfDesign{ get; set; }
-            public string ChooseColor { get; set; }
-        public int SizeOfPrint { get; set; }
-        public string Papertype { get; set; }
-        public string DeliveryPickup { get; set; }
-
-            #endregion
+            
 
         }
     }
